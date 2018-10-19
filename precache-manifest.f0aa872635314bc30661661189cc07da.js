eccd22e4cb60bc9450943b4d945d5f94db271c12
Igor Hruscevskii <ihruch@gmail.com>
2018-10-19 14:25:38 +0300

    Updates

---
+++ precache-manifest.f0aa872635314bc30661661189cc07da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "02a9430b88975cae16fcfcc9cf7b5799",
+    "url": "/movies-react/static/media/logo-db.02a9430b.svg"
+  },
+  {
+    "revision": "85f1fd7c8c1f4c8e0bfd",
+    "url": "/movies-react/static/js/runtime~main.85f1fd7c.js"
+  },
+  {
+    "revision": "832077d5394c38520e92",
+    "url": "/movies-react/static/js/main.832077d5.chunk.js"
+  },
+  {
+    "revision": "9f1175ae7a23b3d57940",
+    "url": "/movies-react/static/js/1.9f1175ae.chunk.js"
+  },
+  {
+    "revision": "832077d5394c38520e92",
+    "url": "/movies-react/static/css/main.44290c86.chunk.css"
+  },
+  {
+    "revision": "7361c0ed196c379d778f791e438f9780",
+    "url": "/movies-react/index.html"
+  }
+];
